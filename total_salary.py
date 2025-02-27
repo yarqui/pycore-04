c61197d6f0946e2a35a306c63bc27766a88f44b1
yarqui <y.pelykh@gmail.com>
2025-02-26 21:13:38 -0500

    add total_salary func

---
+++ total_salary.py
@@ -0,0 +1,18 @@
+def total_salary(path: str) -> tuple | None:
+    try:
+        if type(path) is not str:
+            raise TypeError('Path should be a string')
+
+        with open(path, encoding='UTF-8') as fh:
+            lines = [line.strip() for line in fh] #readlines is less efficient with memory as it loads all lines into memory at once
+            number_of_employees = len(lines)
+            total_salary = sum(int(line.split(',')[1]) for line in lines)
+            average_salary = total_salary // number_of_employees if number_of_employees > 0 else 0
+
+        return total_salary, average_salary 
+
+    except Exception as err:
+        print(f'{type(err)}: {err}')
+        return None
+
+print(total_salary('employees.txt'))
